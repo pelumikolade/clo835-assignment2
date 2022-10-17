9c2fc7aba6f41c645651358a6ac80bbea76c9864
Oluwapelumi <pelumi.kolade@gmail.com>
2022-10-17 03:07:43 +0000

    updated the key name for ec2

--- ec2.tf
+++ ec2.tf
@@ -43,13 +43,13 @@ resource "aws_instance" "k8s" {
     project = "clo835"
   }
 
-  key_name                = "week5"
+  key_name                = "assignment2"
   monitoring              = true
   disable_api_termination = false
   ebs_optimized           = true
 }
 
 resource "aws_key_pair" "k8s" {
-  key_name   = "week5"
-  public_key = file("${path.module}/week5.pub")
+  key_name   = "assignment2"
+  public_key = file("${path.module}/assignment2.pub")
 }
